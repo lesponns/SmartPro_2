197d432c40d8848238ff5fd30edc14349df41c6b
lesponns <lesponns@DESKTOP-44MF2L6>
2023-07-20 19:04:17 +0300

    Proje dosyası ekle.

---
+++ SmartPro_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Koşul İfadeleri
+            /*
+             
+            == Eşittir
+            === Denktir
+            < Küçüktür
+            > Büyüktür
+            <= Küçük veya Eşittir
+            >= Büyük veya Eşittir
+            && Ve
+            || Veya
+            != Eşit Değildir veya Denk Değildir
+
+            */
+            #endregion
+
+
+        }
+    }
+}
